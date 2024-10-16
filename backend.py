1faa85e671bb98872e658f6f0863e93bc78d15e6
SrmxKub <158071996+SrmxKub@users.noreply.github.com>
2024-10-17 01:28:18 +0700

    Add files via upload

--- backend.py
+++ backend.py
@@ -13,14 +13,15 @@ df['num_seller_ratings'] = df['num_seller_ratings'].apply(
 
 df['num_seller_ratings'] = df['num_seller_ratings'].fillna(0).astype(int)
 
+# Cosine Similarity Calculation
 def cosine_similarity_cal(feature, filtered_data, input_value):
 
     input_df = pd.DataFrame({feature: [input_value]})
     combined_df = pd.concat([filtered_data[[feature]], input_df])
     tfidf_matrix = TfidfVectorizer().fit_transform(combined_df[feature])
-
     return cosine_similarity(tfidf_matrix[-1], tfidf_matrix[:-1]).flatten()
 
+# Find most similar perfume
 def most_similar_perfumes(scents, base_notes, middle_notes, typ, min_price, max_price, top_n = 10):
     
     filtered_data = df[(min_price <= df['price_THB']) & (df['price_THB'] <= max_price)]
@@ -35,7 +36,7 @@ def most_similar_perfumes(scents, base_notes, middle_notes, typ, min_price, max_
     base_note_sim = cosine_similarity_cal('base_note', filtered_data, base_notes)
     middle_note_sim = cosine_similarity_cal('middle_note', filtered_data, middle_notes)
     
-    similarity = (scent_sim + base_note_sim + middle_note_sim + 2) / 5 # +2 are values of type of perfumes and price
+    similarity = (scent_sim + base_note_sim + middle_note_sim) / 3
 
     data = filtered_data.copy()
     data['cosine_scent'] = scent_sim
@@ -48,6 +49,8 @@ def most_similar_perfumes(scents, base_notes, middle_notes, typ, min_price, max_
 
     return top_similar_perfumes[['name', 'brand', 'department', 'price_THB', 'ml', 'item_rating', 'cosine_similarity', 'img_url']]
 
+
+# Create heatmap with perfume type
 def get_heatmap():
     departments = ['Men', 'Women', 'Unisex', 'All']
     
